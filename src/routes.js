3fd4299bc1491bd596eebe9965419eeb5df434d5
Iago Melo <iagosilvamelo@gmail.com>
2019-09-10 11:07:32 -0300

    Criado métodos CRUD em ProductController.

--- src/routes.js
+++ src/routes.js
@@ -10,5 +10,9 @@ routes.get('/', (req, res) => {
 const ProductController = require('./Controllers/ProductController');
 
 routes.get('/Product', ProductController.index);
+routes.get('/Product/:id', ProductController.show);
+routes.post('/Product', ProductController.store);
+routes.put('/Product/:id', ProductController.update);
+routes.delete('/Product/:id', ProductController.destroy);
 
 module.exports = routes;
