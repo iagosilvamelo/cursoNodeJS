469dec28391978e07003acfdd758361988c4963a
Iago Melo <iagosilvamelo@gmail.com>
2019-09-10 09:44:12 -0300

    Instalado e configurado Mongoose para ORM do mongoDB.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
+
+// Router
 app.get('/', (req, res) => {
 	res.send('Helo Iago');
 });
 
+// Porta para acesso do app
 app.listen(3001);
